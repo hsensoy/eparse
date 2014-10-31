aac7d4537b4910c844d02340b7bca188d93cca9a
Husnu Sensoy <hsensoy@ku.edu.tr>
2014-10-31 22:39:05 +0200

    Bundle data directory

--- bundle.sh
+++ bundle.sh
@@ -1,1 +1,3 @@
-git archive --format=tar.gz --prefix=eparse-$1/ v$1 >eparse-$1.tar.gz
+git archive --format=tar --prefix=eparse-$1/ v$1 >eparse-$1.tar
+tar --append --file=eparse-$1.tar data
+gzip eparse-$1.tar
