15db902b8d6d2ef9c7c342090fc82a81ed4350a0
Husnu Sensoy <hsensoy@ku.edu.tr>
2015-04-28 18:27:52 +0300

    Minor CMakeLists.txt and scripts/run_rbffeat.sh changes.

--- scripts/run_rbffeat.sh
+++ scripts/run_rbffeat.sh
@@ -1,4 +1,4 @@
 source /opt/intel/mkl/bin/mklvars.sh intel64
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib/perceptronmkl
 
-eparse -t 2-22 -d 22 -o opt.rbf.feat.fixed -p /Users/husnusensoy/data/conllWSJToken_wikipedia2MUNK-25-fixed -s optimize -e p-1v_p0v_p1v_c-1v_c0v_c1v_tl_lbf_rbf_root_between -l 50 -x RBF -z .5 -v 0 -c 2 -f 3650
+eparse -t 2-22 -d 22 -o opt.rbf.feat.fixed -p /home/ubuntu/data/conllWSJToken_wikipedia2MUNK-25-fixed -s optimize -e p-1v_p0v_p1v_c-1v_c0v_c1v_tl_lbf_rbf_root_between -l 50 -x RBF -z .5 -v 0 -c 2 -f 20000
